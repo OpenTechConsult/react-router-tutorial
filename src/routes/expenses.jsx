96ad06c25ab5da721a940464eded3ee58480ff89
OpenTech-Consult <opentech.consult@gmail.com>
2022-05-25 15:46:08 +0000

    Add some routes
    Added a couple of new files
    
    - created two jsx  files (src/routes/invoices.jsx and src/routes/expenses) along with their css files
    - instruct react router how to render the app at different url by creating our first 'Route Config' inside index.js
    
    closes #4

---
+++ src/routes/expenses.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './expenses.css'
+
+const Expenses = () => {
+    return (
+        <main className="App" style={{ padding: '1rem 0' }}>
+            <h2>Expenses</h2>
+        </main>
+    )
+}
+
+export default Expenses
