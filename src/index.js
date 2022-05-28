7517221012832bce879d35bd69770cbfffd37a20
OpenTech-Consult <opentech.consult@gmail.com>
2022-05-28 13:25:49 +0000

    Reading URL params
    Added a route for a specific invoice
    
    - created a new component at src/routes/invoice.jsx
    - defined a route that will match URLs like /invoices/1998 or /invoices/2005 and that will enable us to get the invoice number from it.
    - create a new Route inside the "invoice" route
    - added an <Outlet/> to the parent layout rouyte
    - build a more interesting invoice
    - added a new function to lookup invoices by their number in src/data.js
    - in src/routes/invoice.jsx
    
    closes #14

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App';
 import Expenses from './routes/expenses';
 import Invoices from './routes/invoices';
+import Invoice from './routes/invoice';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -12,7 +13,9 @@ root.render(
     <Routes>
       <Route path="/" element={<App />}>
         <Route path="expenses" element={<Expenses />} />
-        <Route path="invoices" element={<Invoices />} />
+        <Route path="invoices" element={<Invoices />}>
+          <Route path=":invoiceId" element={<Invoice />} />
+        </Route>
         <Route path="*" element={
           <main style={{ padding: "1rem" }}>
             <p>There is nothing here!</p>
