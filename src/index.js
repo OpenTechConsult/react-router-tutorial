4c2638c3e1a5d76b9d9990c2caf5231f91db26c4
OpenTech-Consult <opentech.consult@gmail.com>
2022-05-28 05:46:17 +0000

    Adding a "No Match" Route
    If none of the route we define match the URL a link points to, we want a "no match" route to be defined.
    
    In the index.js add a Route component with path prop set to *
    
    closes #13

--- src/index.js
+++ src/index.js
@@ -13,6 +13,11 @@ root.render(
       <Route path="/" element={<App />}>
         <Route path="expenses" element={<Expenses />} />
         <Route path="invoices" element={<Invoices />} />
+        <Route path="*" element={
+          <main style={{ padding: "1rem" }}>
+            <p>There is nothing here!</p>
+          </main>
+        } />
       </Route>
     </Routes>
   </BrowserRouter>
